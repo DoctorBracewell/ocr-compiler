536bad1adb14256e2842f0735c1f929e28f46339
DrBracewell <doctorbracewell@gmail.com>
2022-06-06 23:30:56 +0100

    Grammar updates + Relevant tests

--- packages/rust-compiler/src/lib.rs
+++ packages/rust-compiler/src/lib.rs
@@ -18,17 +18,32 @@ mod tests {
 
     #[test]
     fn parse_padded_linebreaks() {
-        parse("\n\n\n// This is a comment statement!\n\n\n").unwrap();
+        let result = parse("\n\n\n// This is a comment statement!\n\n\n").unwrap();
+        dbg!(result);
+    }
+
+    #[test]
+    fn parse_padded_statement() {
+        let result = parse("    \n  // This is a comment statement! \n  ").unwrap();
+        dbg!(result);
     }
 
     #[test]
     fn parse_comment() {
-        parse("// This is a comment statement!").unwrap();
+        let result = parse("// This is a comment statement!").unwrap();
+        dbg!(result);
     }
 
     #[test]
     fn parse_assignment() {
-        parse("// This is a comment statement!").unwrap();
-        parse("// This is a comment statement!").unwrap();
+        parse("variable_name = test").unwrap();
+        parse("const variable_name1=test").unwrap();
+        parse("global  variableName  =  test").unwrap();
     }
+
+    // #[test]
+    // fn debug() {
+    //     dbg!(parse("//comment!").unwrap());
+    //     panic!("");
+    // }
 }
