e5d9c75bc93243677c28b8fb80bbc78f07c86230
DrBracewell <doctorbracewell@gmail.com>
2022-06-06 18:20:53 +0100

    Added main site template

---
+++ apps/svelte-site/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Counter from "./components/Counter.svelte";
+</script>
+
+<main>
+	<h1>Hello Typescript!</h1>
+
+	<Counter />
+
+	<p>
+		Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.
+	</p>
+
+	<p>
+		Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for the officially supported
+		framework, also powered by Vite!
+	</p>
+</main>
