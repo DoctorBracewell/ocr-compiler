12c59466ee2dd64ba6f22ec50dd53005d77532ac
DrBracewell <doctorbracewell@gmail.com>
2022-06-22 01:59:29 +0100

    Mock editor updates (Actions Testing)
    Bumped to

--- packages/editor/package/components/Editor.svelte
+++ packages/editor/package/components/Editor.svelte
@@ -2,8 +2,8 @@
 	//@ts-ignore
 	import init, { print_parse } from "@ocr-compiler/compiler";
 
+	//@ts-ignore
 	init(import.meta.env.DEV ? "compiler_bg.wasm" : undefined).then(() => {
-		console.log("init wasm-pack");
 		console.log(print_parse("from"));
 	});
 </script>
