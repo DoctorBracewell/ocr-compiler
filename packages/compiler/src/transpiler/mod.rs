d264f5a4f11192b99e312e576d597530f11a518f
DrBracewell <doctorbracewell@gmail.com>
2022-07-23 14:42:24 +0100

    Bug fixes

--- packages/compiler/src/transpiler/mod.rs
+++ packages/compiler/src/transpiler/mod.rs
@@ -11,41 +11,31 @@ mod transpiler_data;
 use transpiler_data::*;
 
 // Symbol table entry, one per identifier
-#[derive(Default, Debug)]
+#[derive(Debug, Clone)]
 struct SymbolTableEntry {
     minified_name: String,
     constant: bool,
 }
 
-// Symbol table, keeps tracking of
-#[derive(Default, Debug)]
-struct SymbolTable {
-    table: HashMap<String, SymbolTableEntry>,
-    naming: u32,
+fn add_table_entry(table: &mut SymbolTable, identifier: &str) -> Result<(), TranspilerError> {
+    let identifier_copy = identifier.to_string();
+
+    table.insert(
+        identifier_copy,
+        SymbolTableEntry {
+            minified_name: denary_to_alphabet(table.len().try_into().unwrap()),
+            constant: false,
+        },
+    );
+
+    Ok(())
 }
 
-impl SymbolTable {
-    fn add_entry(&mut self, identifier: &str) -> Result<(), TranspilerError> {
-        let identifier_copy = identifier.to_string();
-
-        self.table.insert(
-            identifier_copy,
-            SymbolTableEntry {
-                minified_name: denary_to_alphabet(self.table.len().try_into().unwrap()),
-                constant: false,
-            },
-        );
-
-        self.naming += 1;
-
-        Ok(())
-    }
-}
+type SymbolTable = HashMap<String, SymbolTableEntry>;
 
 #[derive(Default, Debug)]
 struct JavascriptProgram {
     text: String,
-    symbol_table: SymbolTable,
 }
 
 impl JavascriptProgram {
@@ -53,7 +43,7 @@ impl JavascriptProgram {
         self.text.push_str(text);
     }
 
-    fn queue_text(&mut self, text: String) {
+    fn prepend_text(&mut self, text: String) {
         self.text = text + &self.text;
     }
 }
@@ -67,27 +57,22 @@ pub fn transpile(rules: Pairs<Rule>) -> String {
 
 fn handle_errors(error: TranspilerError) -> String {
     dbg!(error);
-    String::from("j")
+    String::from("error")
 }
 
 fn transpilation_stages(mut rules: Pairs<Rule>) -> Result<String, TranspilerError> {
     // Initialise program
     let program_rule = rules.next().ok_or(TranspilationError)?;
     let mut result = JavascriptProgram::default();
+    let mut symbol_table: SymbolTable = HashMap::default();
 
     // Traverse program rules to generate program
-    program(program_rule, &mut result)?;
+    program(program_rule, &mut result, &mut symbol_table)?;
 
     // Initiliase variables
-    result.queue_text(
-        &(result
-            .symbol_table
-            .table)
-            .into_values()
-            .fold(String::new(), |acc, s| {
-                acc + &format!("{}{}{}{}", LET, SPACE, s.minified_name, SEMICOLON)
-            })
-    );
+    result.prepend_text(symbol_table.into_values().fold(String::new(), |acc, s| {
+        acc + &format!("{}{}{}{}", LET, SPACE, s.minified_name, SEMICOLON)
+    }));
 
     Ok(result.text)
 }
@@ -95,10 +80,11 @@ fn transpilation_stages(mut rules: Pairs<Rule>) -> Result<String, TranspilerErro
 fn program(
     program_rule: Pair<Rule>,
     result: &mut JavascriptProgram,
+    symbol_table: &mut SymbolTable,
 ) -> Result<(), TranspilerError> {
     for rule in program_rule.into_inner() {
         match rule.as_rule() {
-            Rule::block => block(rule, result)?,
+            Rule::block => block(rule, result, symbol_table)?,
             Rule::EOI => (),
             _ => unreachable!(),
         }
@@ -107,10 +93,14 @@ fn program(
     Ok(())
 }
 
-fn block(block: Pair<Rule>, result: &mut JavascriptProgram) -> Result<(), TranspilerError> {
+fn block(
+    block: Pair<Rule>,
+    result: &mut JavascriptProgram,
+    symbol_table: &mut SymbolTable,
+) -> Result<(), TranspilerError> {
     for statement in block.into_inner() {
         match statement.as_rule() {
-            Rule::assignment => assignment(statement, result)?,
+            Rule::assignment => assignment(statement, result, symbol_table)?,
             Rule::forLoop => unimplemented!(),
             Rule::ifStatement => unimplemented!(),
             Rule::expression => unimplemented!(),
@@ -124,6 +114,7 @@ fn block(block: Pair<Rule>, result: &mut JavascriptProgram) -> Result<(), Transp
 fn assignment(
     assignment: Pair<Rule>,
     result: &mut JavascriptProgram,
+    symbol_table: &mut SymbolTable,
 ) -> Result<(), TranspilerError> {
     let assignment_type = assignment.into_inner().next().ok_or(IteratorError)?;
 
@@ -131,7 +122,7 @@ fn assignment(
         Rule::arrayDeclaration => unimplemented!(),
         Rule::arrayAssignment => unimplemented!(),
         Rule::taggedVariableAssignment => unimplemented!(),
-        Rule::variableAssignment => variable_assignment(assignment_type, result)?,
+        Rule::variableAssignment => variable_assignment(assignment_type, result, symbol_table)?,
 
         _ => unreachable!(),
     };
@@ -142,22 +133,20 @@ fn assignment(
 fn variable_assignment(
     assignment: Pair<Rule>,
     result: &mut JavascriptProgram,
+    symbol_table: &mut SymbolTable,
 ) -> Result<(), TranspilerError> {
     // Extract data from statement
     let mut parts = assignment.into_inner();
     let identifier = parts.next().ok_or(IteratorError)?.as_str().to_string();
 
     // Add new variable to the symbol table
-    if !result.symbol_table.table.contains_key(&identifier) {
-        result.symbol_table.add_entry(&identifier)?;
+    if !symbol_table.contains_key(&identifier) {
+        add_table_entry(symbol_table, &identifier)?;
     }
 
     // Get minified name from symbol table and push to result
-    let SymbolTableEntry { minified_name, .. } = result
-        .symbol_table
-        .table
-        .get(&identifier)
-        .ok_or(TranspilationError)?;
+    let SymbolTableEntry { minified_name, .. } =
+        symbol_table.get(&identifier).ok_or(TranspilationError)?;
 
     result.push_text(&format!("{}{}", minified_name, ASSIGNMENT_OPERATOR));
 
